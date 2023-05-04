c2cead8af7e586914797663893e68449be8ecfe3
dfralan <dfralan@gmail.com>
2023-05-04 11:07:42 -0300

    Initial Commit

---
+++ a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by Alan D on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
